84b07a48c5fbcd974fa93313dd32b13d64709941
JJ <unknown@example.com>
2019-06-21 16:40:39 +0800

    use route attribute

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace asp_net_core_cli.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
+        [Route("Home")]
         public string Index()
         {
             return "This is Home/Index";
         }
+        [Route("[action]")]
         public string About()
         {
             return "This is Home/About";
