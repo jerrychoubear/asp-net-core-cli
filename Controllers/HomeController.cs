bc938e4a0133273bc215965bc49addd3474e433a
JJ <unknown@example.com>
2019-06-21 16:42:03 +0800

    use action-only route attribute

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace asp_net_core_cli.Controllers
 {
-    [Route("[controller]")]
     public class HomeController : Controller
     {
-        [Route("Home")]
         public string Index()
         {
             return "This is Home/Index";
